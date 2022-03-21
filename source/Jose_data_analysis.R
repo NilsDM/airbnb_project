e3d0ece1ef69c5b91adf84cf31c80f167a9f96a3
jmlama29 <90584384+jmlama29@users.noreply.github.com>
2022-03-20 22:30:56 -0400

    plots added

--- source/Jose_data_analysis.R
+++ source/Jose_data_analysis.R
@@ -16,8 +16,10 @@ source("source/db_prep.R")
 # Libraries
 library(tidyr)
 library(dbplyr)
-
-
+library(knitr)
+library(scales)
+library(ggplot2)
+library(gridExtra)
 
 #################### Database Connection
 con <- dbConnect(RSQLite::SQLite(), "airdb.SQLite")
@@ -38,35 +40,128 @@ dbClearResult(res_listing)
 
 
 
-#################### Query 1: What is the top and bottom 10 property type based on average price? (Room Listing)
+#################### Query 1: What is the top and bottom 10 property type based on average review_scores_rating? (Room Listing)
  
 q1<- listing%>%
-    select(property_type,price)%>%
+    select(property_type,review_scores_rating)%>%
     group_by(property_type)%>%
-    summarise(price = mean(price))
-
+    summarise(review_scores_rating = mean(review_scores_rating))%>%
+    arrange(desc(review_scores_rating))
+        
 top_q1<- q1%>%
-    arrange(desc(price))%>%
-    top_n(10)
-    
+    arrange(desc(review_scores_rating))%>%
+    top_n(10)%>% 
+    ggplot(aes(x = review_scores_rating, y = property_type %>% reorder(review_scores_rating))) + 
+    geom_col(fill = "Aquamarine4") + 
+   
+    scale_x_continuous(labels = scales::number_format(big.mark = ",")) + 
+    labs(
+        title    = "Top Property Type by Average review_scores_rating",
+        x        = "Average review_scores_rating",
+        y        = "Property Type"
+    ) + 
+    theme_tq() + 
+    theme(axis.text.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold"))
+
+
+top_property_type_average_review_scores_rating <-q1 %>%
+    arrange(desc(review_scores_rating))%>%
+    top_n(10)%>% 
+    knitr::kable(align = c("l", "c"),
+    format.args = list(big.mark = ","),
+    digits = 2)
+
+top_property_type_average_review_scores_rating
+
+
 bottom_q1<- q1%>%
-    arrange(price)%>%
-    top_n(-10)
+    arrange((review_scores_rating))%>%
+    top_n(-10)%>% 
+    ggplot(aes(x = review_scores_rating, y = property_type %>% reorder(review_scores_rating))) + 
+    geom_col(fill = "Skyblue3") + 
+    scale_x_continuous(labels = scales::number_format(big.mark = ",")) + 
+    labs(
+        title    = "Bottom Property Type by Average review_scores_rating",
+        x        = "Average review_scores_rating",
+        y        = "Property Type"
+    ) + 
+    theme_tq() + 
+    theme(axis.text.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold")) 
+
+bottom_property_type_average_review_scores_rating <-q1 %>%
+    arrange(review_scores_rating)%>%
+    top_n(-10)%>% 
+    knitr::kable(align = c("l", "c"),
+                 format.args = list(big.mark = ","),
+                 digits = 2)
+
+bottom_property_type_average_review_scores_rating
+
+grid.arrange(top_q1, bottom_q1, ncol = 2)
 
 #################### Query 2: What is the top and bottom 10 property type based on review score? (Room Listing)
 
 q2<- listing%>%
     select(property_type,review_scores_rating)%>%
     group_by(property_type)%>%
-    summarise(review_scores_rating = mean(review_scores_rating))
+    summarise(review_scores_rating = mean(review_scores_rating))%>%
+    arrange(desc(review_scores_rating))
 
 top_q2<- q2%>%
     arrange(desc(review_scores_rating))%>%
-    top_n(10)
+    top_n(10)%>% 
+    ggplot(aes(x = review_scores_rating, y = property_type %>% reorder(review_scores_rating))) + 
+    geom_col(fill = "Aquamarine4") + 
+    
+    scale_x_continuous(labels = scales::number_format(big.mark = ",")) + 
+    labs(
+        title    = "Top Property Type by Review Score Rating",
+        x        = "Review Score Rating",
+        y        = "Property Type"
+    ) + 
+    theme_tq() + 
+    theme(axis.text.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold"))
+
+
+top_property_type_review_scores_rating <-q2 %>%
+    arrange(desc(review_scores_rating))%>%
+    top_n(10)%>% 
+    knitr::kable(align = c("l", "c"),
+                 format.args = list(big.mark = ","),
+                 digits = 2)
+
+top_property_type_review_scores_rating
+
 
 bottom_q2<- q2%>%
+    arrange((review_scores_rating))%>%
+    top_n(-10)%>% 
+    ggplot(aes(x = review_scores_rating, y = property_type %>% reorder(review_scores_rating))) + 
+    geom_col(fill = "Skyblue3") + 
+    scale_x_continuous(labels = scales::number_format(big.mark = ",")) + 
+    labs(
+        title    = "Bottom Property Type by Review Score Rating",
+        x        = "Review Score Rating",
+        y        = "Property Type"
+    ) + 
+    theme_tq() + 
+    theme(axis.text.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold")) 
+
+bottom_property_type_review_scores_rating <-q2 %>%
     arrange(review_scores_rating)%>%
-    top_n(-10)
+    top_n(-10)%>% 
+    knitr::kable(align = c("l", "c"),
+                 format.args = list(big.mark = ","),
+                 digits = 2)
+
+bottom_property_type_review_scores_rating
+
+grid.arrange(top_q2, bottom_q2, ncol = 2)
+
 
 
 #################### Query 3: What is the most common amenities provided? (Room Listing)
@@ -89,16 +184,22 @@ q3<- pivot_longer(data=q3,
 q3<- q3%>%
     select(separated_amenities)%>%
     group_by(separated_amenities)%>%
-    summarise("amenities_count"=n())
+    summarise(amenities_count=n())
+
 
 most_common_amenities<- q3%>%
      na.omit()%>%
      arrange(desc(amenities_count))%>%
-     top_n (1)
+     top_n (1)%>% 
+    knitr::kable(align = c("l", "c"),
+                 format.args = list(big.mark = ","),
+                 digits = 2)
+
+most_common_amenities
 
 #################### Query 4: What is the most commonly verified host information? (Host)
 
-ncols_q4 <- max(stringr::str_count(host_info$host_verifications, ",")) + 1
+ncols_q4 <- max(stringr::str_count(na.omit(host_info$host_verifications), ",")) + 1
 colmn_q4 <- paste("col", 1:ncols_q4)
 
 q4<- host_info%>%
@@ -109,31 +210,47 @@ q4<- host_info%>%
         into=colmn_q4,
         remove = FALSE)
 
-q4<- pivot_longer(data=q3,
-                  cols = 'col 1':'col 166',
+q4<- pivot_longer(data=q4,
+                  cols = 'col 1':'col 12',
                   names_to = "col_number",
                   values_to = "separated_host_verifications")
 
 q4<- q4%>%
     select(separated_host_verifications)%>%
     group_by(separated_host_verifications)%>%
-    summarise("host_verifications_count"=n())
+    summarise(host_verifications_count=n())
 
 most_common_host_verifications<- q4%>%
     na.omit()%>%
     arrange(desc(host_verifications_count))%>%
-    top_n (1)
+    top_n (1)%>% 
+    knitr::kable(align = c("l", "c"),
+                 format.args = list(big.mark = ","),
+                 digits = 2)
 
-#verificar error
+most_common_host_verifications
 
 
 #################### Query 5: What is the most common room type available? (Room Listing)
 
+
 q5<- listing%>%
+    select(room_type,has_availability)%>%
     group_by(room_type)%>%
-    summarise(n())%>%
-    top_n(1)
+    filter(has_availability==1)%>%
+    summarise(availability=n())%>% 
+    top_n (1)%>% 
+    knitr::kable(align = c("l", "c"),
+                 format.args = list(big.mark = ","),
+                 digits = 2)
 
-#has_availability
+most_common_room_type_available <-q5
 
-
+most_common_room_type_available
+###########################################################################################
+    
+    
+    
+    
+    
+    
