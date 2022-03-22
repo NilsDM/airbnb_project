b1ad35b92d8331e77b052e29db4d0890086969b1
jmlama29 <90584384+jmlama29@users.noreply.github.com>
2022-03-21 21:47:23 -0400

    all plots

--- source/Jose_data_analysis.R
+++ source/Jose_data_analysis.R
@@ -15,11 +15,12 @@ source("source/db_prep.R")
 
 # Libraries
 library(tidyr)
-library(dbplyr)
+library(dplyr)
 library(knitr)
 library(scales)
 library(ggplot2)
 library(gridExtra)
+library(tibble)
 
 #################### Database Connection
 con <- dbConnect(RSQLite::SQLite(), "airdb.SQLite")
@@ -40,24 +41,24 @@ dbClearResult(res_listing)
 
 
 
-#################### Query 1: What is the top and bottom 10 property type based on average review_scores_rating? (Room Listing)
- 
+#################### Query 1: What is the top and bottom 10 property type based on average price? (Room Listing)
+
 q1<- listing%>%
-    select(property_type,review_scores_rating)%>%
+    select(property_type,price)%>%
     group_by(property_type)%>%
-    summarise(review_scores_rating = mean(review_scores_rating))%>%
-    arrange(desc(review_scores_rating))
+    summarise(price = mean(price))%>%
+    arrange(desc(price))
         
 top_q1<- q1%>%
-    arrange(desc(review_scores_rating))%>%
+    arrange(desc(price))%>%
     top_n(10)%>% 
-    ggplot(aes(x = review_scores_rating, y = property_type %>% reorder(review_scores_rating))) + 
+    ggplot(aes(x = price, y = property_type %>% reorder(price))) + 
     geom_col(fill = "Aquamarine4") + 
    
     scale_x_continuous(labels = scales::number_format(big.mark = ",")) + 
     labs(
-        title    = "Top Property Type by Average review_scores_rating",
-        x        = "Average review_scores_rating",
+        title    = "Top Property Type by Average price",
+        x        = "Average price",
         y        = "Property Type"
     ) + 
     theme_tq() + 
@@ -65,39 +66,39 @@ top_q1<- q1%>%
           axis.text.y = element_text(face = "bold"))
 
 
-top_property_type_average_review_scores_rating <-q1 %>%
-    arrange(desc(review_scores_rating))%>%
+top_property_type_average_price <-q1 %>%
+    arrange(desc(price))%>%
     top_n(10)%>% 
     knitr::kable(align = c("l", "c"),
     format.args = list(big.mark = ","),
     digits = 2)
 
-top_property_type_average_review_scores_rating
+top_property_type_average_price
 
 
 bottom_q1<- q1%>%
-    arrange((review_scores_rating))%>%
+    arrange((price))%>%
     top_n(-10)%>% 
-    ggplot(aes(x = review_scores_rating, y = property_type %>% reorder(review_scores_rating))) + 
+    ggplot(aes(x = price, y = property_type %>% reorder(price))) + 
     geom_col(fill = "Skyblue3") + 
     scale_x_continuous(labels = scales::number_format(big.mark = ",")) + 
     labs(
-        title    = "Bottom Property Type by Average review_scores_rating",
-        x        = "Average review_scores_rating",
+        title    = "Bottom Property Type by Average price",
+        x        = "Average price",
         y        = "Property Type"
     ) + 
     theme_tq() + 
     theme(axis.text.x = element_text(face = "bold"),
           axis.text.y = element_text(face = "bold")) 
 
-bottom_property_type_average_review_scores_rating <-q1 %>%
-    arrange(review_scores_rating)%>%
+bottom_property_type_average_price <-q1 %>%
+    arrange(price)%>%
     top_n(-10)%>% 
     knitr::kable(align = c("l", "c"),
                  format.args = list(big.mark = ","),
                  digits = 2)
 
-bottom_property_type_average_review_scores_rating
+bottom_property_type_average_price
 
 grid.arrange(top_q1, bottom_q1, ncol = 2)
 
@@ -184,13 +185,27 @@ q3<- pivot_longer(data=q3,
 q3<- q3%>%
     select(separated_amenities)%>%
     group_by(separated_amenities)%>%
-    summarise(amenities_count=n())
+    summarise(amenities_count=n())%>%
+    na.omit()%>%
+    arrange(desc(amenities_count))%>%
+    top_n (15)
 
+ 
+ q3_plot<-q3%>%
+    ggplot(aes(x = amenities_count, y = separated_amenities %>% reorder(amenities_count))) + 
+    geom_col(fill = "Aquamarine4") + 
+    
+    scale_x_continuous(labels = scales::number_format(big.mark = ",")) + 
+    labs(
+        title    = "Most Common Amenities",
+        x        = "Count",
+        y        = "Amenities"
+    ) + 
+    theme_tq() + 
+    theme(axis.text.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold"))
 
 most_common_amenities<- q3%>%
-     na.omit()%>%
-     arrange(desc(amenities_count))%>%
-     top_n (1)%>% 
     knitr::kable(align = c("l", "c"),
                  format.args = list(big.mark = ","),
                  digits = 2)
@@ -218,12 +233,29 @@ q4<- pivot_longer(data=q4,
 q4<- q4%>%
     select(separated_host_verifications)%>%
     group_by(separated_host_verifications)%>%
-    summarise(host_verifications_count=n())
-
-most_common_host_verifications<- q4%>%
+    summarise(host_verifications_count=n())%>%
     na.omit()%>%
     arrange(desc(host_verifications_count))%>%
-    top_n (1)%>% 
+    top_n (10)
+
+
+q4_plot<-q4%>%
+    ggplot(aes(x = host_verifications_count, y = separated_host_verifications %>% reorder(host_verifications_count))) + 
+    geom_col(fill = "Aquamarine4") + 
+    scale_x_continuous(labels = scales::number_format(big.mark = ",")) + 
+    labs(
+        title    = "Most Common Verified Information",
+        x        = "Count",
+        y        = "Verified Information"
+    ) + 
+    theme_tq() + 
+    theme(axis.text.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold"))
+
+q4_plot
+
+
+q4 <-most_common_host_verifications<- q4%>%
     knitr::kable(align = c("l", "c"),
                  format.args = list(big.mark = ","),
                  digits = 2)
@@ -238,13 +270,30 @@ q5<- listing%>%
     select(room_type,has_availability)%>%
     group_by(room_type)%>%
     filter(has_availability==1)%>%
-    summarise(availability=n())%>% 
-    top_n (1)%>% 
+    summarise(availability=n())%>%
+    arrange(desc(availability))
+
+
+q5_plot<-q5%>%
+    ggplot(aes(x = availability, y = room_type %>% reorder(availability))) + 
+    geom_col(fill = "Aquamarine4") + 
+    scale_x_continuous(labels = scales::number_format(big.mark = ",")) + 
+    labs(
+        title    = "Most Common Room Type Available",
+        x        = "Availability",
+        y        = "Room Type"
+    ) + 
+    theme_tq() + 
+    theme(axis.text.x = element_text(face = "bold"),
+          axis.text.y = element_text(face = "bold"))
+
+q5_plot
+
+
+most_common_room_type_available <-q5%>% 
     knitr::kable(align = c("l", "c"),
                  format.args = list(big.mark = ","),
                  digits = 2)
-
-most_common_room_type_available <-q5
 
 most_common_room_type_available
 ###########################################################################################
